3bed3867c81ebd370333d49583191479f25dadbd
maisam-mi <riomaisam@gmail.com>
2024-02-22 10:01:35 +0100

    client: adding the records (distance, time, calories and date saving in variable) and edit the profile and creating account page Problem: geolocation doesnt work offline
    Signed-off-by: maisam-mi <riomaisam@gmail.com>

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,28 +2,42 @@
   <div class="justify-center robotoBold">
     <div class="text-center">
       <div class="q-pa-md q-gutter-sm">
-        <q-btn color="blue" class="robotoBold text-h6 q-pa-lg" label="Start" />
+        <q-btn
+          v-if="clicked"
+          color="blue"
+          class="robotoBold text-h6 q-pa-lg"
+          label="Stop"
+          @click="stopRunning()"
+        />
+        <q-btn
+          v-else
+          color="blue"
+          class="robotoBold text-h6 q-pa-lg"
+          label="Start"
+          @click="startRunning()"
+        />
       </div>
     </div>
-    <q-card bordered grid class="my-card bg-orange-5">
+    <div v-if="store.currentRecord == null">Bevore running, please click the start!</div>
+    <q-card v-if="store.currentRecord != null" bordered grid class="my-card bg-orange-5">
       <q-card-section>
-        <div class="text-h6">120000 Kalorien</div>
+        <div class="text-h6">{{ store.currentRecord.calories }} Kalorien</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        <div class="text-body1">2356m</div>
-        <div class="text-body1">30 min</div>
+        <div class="text-body1">{{ store.currentRecord.distance }} m</div>
+        <div class="text-body1">{{ store.currentRecord.time }} min</div>
       </q-card-section>
 
       <q-card-section>
-        <div class="text-caption">13.10.2023</div>
+        <div class="text-caption">{{ store.currentRecord.date }}</div>
       </q-card-section>
     </q-card>
     <div class="q-pa-md">
       <q-table
         grid
         card-class="bg-orange-5 text-dark"
-        style="font-family: &quot;RobotoMedium&quot;"
+        style="font-family: 'RobotoMedium'"
         title="Letzte Aufnahmen"
         :columns="columns"
         :rows="store.records"
@@ -36,48 +50,79 @@
 </template>
 
 <script setup>
-import useDefaultStore from "@/stores/defaultStore.js";
-import { ref, onMounted, onBeforeUnmount } from "vue";
-import { getDistance } from "geolib";
+import useDefaultStore from '@/stores/defaultStore.js';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { getDistance } from 'geolib';
 
 const store = useDefaultStore();
 store.getRecords();
 
+const clicked = ref(false);
+
+const startRunning = () => {
+  clicked.value = !clicked.value;
+  navigator.geolocation.getCurrentPosition((location) => {
+    console.log(
+      'Start > latitude:',
+      location.coords.latitude,
+      'longitude:',
+      location.coords.longitude,
+    );
+  });
+  store.currentRecord = {
+    calories: 120000,
+    distance: 2356,
+    time: 30,
+    date: '13.10.2023',
+  };
+};
+
+const stopRunning = () => {
+  clicked.value = !clicked.value;
+  navigator.geolocation.getCurrentPosition((location) => {
+    console.log(
+      'Stop > latitude:',
+      location.coords.latitude,
+      'longitude:',
+      location.coords.longitude,
+    );
+  });
+};
+
 const columns = [
   {
-    name: "calories",
-    align: "left",
-    label: "Calories",
-    field: "calories",
+    name: 'calories',
+    align: 'left',
+    label: 'Calories',
+    field: 'calories',
     sortable: true,
   },
   {
-    name: "distance",
-    align: "left",
-    label: "Distance",
-    field: "traveldistance",
+    name: 'distance',
+    align: 'left',
+    label: 'Distance',
+    field: 'traveldistance',
     sortable: true,
   },
   {
-    name: "time",
-    align: "left",
-    label: "Time",
-    field: "runnedtime",
+    name: 'time',
+    align: 'left',
+    label: 'Time',
+    field: 'runnedtime',
     sortable: true,
   },
   {
-    name: "date",
-    align: "left",
-    label: "Date",
-    field: "runneddate",
+    name: 'date',
+    align: 'left',
+    label: 'Date',
+    field: 'runneddate',
     sortable: true,
   },
 ];
-
 </script>
 
 <style lang="scss" scoped>
 .robotoBold {
-  font-family: "RobotoBold";
+  font-family: 'RobotoBold';
 }
 </style>
--- server/src/model/record.model.js
+++ server/src/model/record.model.js
@@ -1,28 +1,28 @@
-import { query } from "../../boilerplate/db/index.js";
+import { query } from '../../boilerplate/db/index.js';
 
 const dbgetAllRecords = async () => {
-  const { rows } = await query("select * from records");
+  const { rows } = await query('select * from records');
   return rows;
 };
 
 const dbgetPersonRecords = async (req) => {
   const { rows } = await query(
-    "select calories, traveldistance, runnedtime, runneddate from records where pid = $1",
-    [req.params.pid]
+    'select calories, traveldistance, runnedtime, runneddate from records where pid = $1 order by runneddate desc, rid desc',
+    [req.params.pid],
   );
   return rows;
 };
 
 const dbaddRecord = async (req) => {
   const { rows } = await query(
-    "insert into records (traveldistance, runnedtime, calories, pid, runneddate) values ($1, $2, $3, $4, $5) returning *",
+    'insert into records (traveldistance, runnedtime, calories, pid, runneddate) values ($1, $2, $3, $4, $5) returning *',
     [
       req.body.traveldistance,
       req.body.runnedtime,
       req.body.calories,
       req.body.pid,
       req.body.runneddate,
-    ]
+    ],
   );
   return rows[0];
 };
